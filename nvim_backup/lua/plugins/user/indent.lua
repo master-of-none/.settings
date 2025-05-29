a84dc4e9232a55e2be450e3a4a3a4325cff3d127
Shrikrishna Bhat <37535815+master-of-none@users.noreply.github.com>
2025-05-29 16:06:00 +0530

    Add files via upload

---
+++ nvim_backup/lua/plugins/user/indent.lua
@@ -0,0 +1,55 @@
+-- return {
+--   "lukas-reineke/indent-blankline.nvim",
+--   event = { "BufReadPost", "BufNewFile" },
+--   main = "ibl",
+--   opts = {
+--     indent = { char = "│" }, -- Customize indent character
+--     scope = { enabled = true }, -- Scope highlighting
+--   },
+--     config = function(_, opts)
+--     local highlights = {"Whitespace"}
+--     vim.tbl_extend("force", opts, {
+--       -- options that for some reason you couldn't add in the opts field table
+--     })
+--
+--     require("ibl").setup(opts)
+--   end,
+-- }
+--
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPost", "BufNewFile" },
+  main = "ibl",
+  opts = {
+    indent = { char = "│" }, -- Customize indent character
+    scope = { enabled = true }, -- Scope highlighting
+  },
+  config = function(_, opts)
+    -- Ranibow indent code start
+    local highlight = {
+      "RainbowRed",
+      "RainbowYellow",
+      "RainbowBlue",
+      "RainbowOrange",
+      "RainbowGreen",
+      "RainbowViolet",
+      "RainbowCyan",
+    }
+
+    local hooks = require("ibl.hooks")
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowRed",    { fg = "#E06C75" })
+      vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+      vim.api.nvim_set_hl(0, "RainbowBlue",   { fg = "#61AFEF" })
+      vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+      vim.api.nvim_set_hl(0, "RainbowGreen",  { fg = "#98C379" })
+      vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+      vim.api.nvim_set_hl(0, "RainbowCyan",   { fg = "#56B6C2" })
+    end)
+
+    -- Merge highlight into opts
+    opts.indent.highlight = highlight
+    -- Rainbow indent code ends
+    require("ibl").setup(opts)
+  end,
+}
---
+++ nvim_backup/lua/user/options.lua
@@ -0,0 +1,11 @@
+return {
+    opt = {
+        tabstop = 4, -- make tabs 4 spaces wide
+        shiftwidth = 4, -- tab should insert 4 spaces
+        softtabstop=4,
+        expandtab=true,
+        smarttab=true,
+        guifont="FiraCode Nerd Font",
+
+    },
+}
